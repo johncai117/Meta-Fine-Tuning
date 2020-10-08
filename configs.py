ad911a1438fb10863df9d62ea933164816bd1007
bill_cai <bilnomo@gmail.com>
2020-10-08 19:34:10 +0800

    update gitignore and configs

--- configs.py
+++ configs.py
@@ -1,9 +1,9 @@
 save_dir                    = './logs'
 
-miniImageNet_path           	= '/content/miniImagenet3'
+miniImageNet_path           	= 'content/miniImagenet3'
 DTD_path 						= '/ssd/dtd/images/'
 
-ISIC_path = "/content"
-ChestX_path = "/content"
-CropDisease_path = "/content/CropDiseases"
-EuroSAT_path = "/content/2750"
+ISIC_path = "content"
+ChestX_path = "content"
+CropDisease_path = "content/CropDiseases"
+EuroSAT_path = "content/2750"
